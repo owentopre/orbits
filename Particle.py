839a63800002e51e57c324e9284f361a4624a1f0
owentopre <74600877+owentopre@users.noreply.github.com>
2020-12-01 19:56:31 +0000

    Add files via upload

--- Particle.py
+++ Particle.py
@@ -1,10 +1,29 @@
 import math
 import numpy as np
 import copy
+G = 6.67408E-11
 
 class Particle:
     '''
-    placeholder docstring
+    A class representing a particle.
+    
+    Inputs
+    ------
+    position : array
+          A numpy array with 3 floats representing the particle's starting position in 3d space
+    velocity : array
+        A numpy array with 3 floats representing the particle's starting velocity through 3d space
+    acceleration : array
+        A numpy array with 3 floats representing the particle's starting acceleration through 3d space
+    name : str
+        A string that is used as a name for the particle
+    mass : float
+        A floating point number used as the particle's mass
+    G : float
+        The universal gravitational constant
+
+    Methods:
+
     '''
     def __init__(
         self,
@@ -13,14 +32,35 @@ class Particle:
         acceleration=np.array([0, -10, 0], dtype=float),
         name='Ball',
         mass=1.0,
-        G = 6.67408E-11
+        index = 0
         ):
+        """
+        Construsts the parameters of the particle, and gives defaults unless they are overwritten
+
+        Parameters
+        ----------
+            position : array
+                A numpy array with 3 floats representing the particle's starting position in 3d space
+            velocity : array
+                A numpy array with 3 floats representing the particle's starting velocity through 3d space
+            acceleration : array
+                A numpy array with 3 floats representing the particle's starting acceleration through 3d space
+            name : str
+                A string that is used as a name for the particle
+            mass : float
+                A floating point number used as the particle's mass
+            G : float
+                The universal gravitational constant
+            
+        """
         self.position = np.array(position, dtype=float)
         self.velocity = np.array(velocity, dtype=float)
         self.acceleration = np.array(acceleration, dtype=float)
         self.name = name
         self.mass = mass
         self.G = G
+        self.index = index
+        
         
 
     def __str__(self):
@@ -29,13 +69,18 @@ class Particle:
         )
 
     def update(self, deltaT):
+        #temporary as is only Euler algorithm  
         self.position = self.position + self.velocity*deltaT
         self.velocity = self.velocity + self.acceleration*deltaT
 
-    def updateGravitationalAcceleration(self, body):    
+    def updateGravitationalAcceleration(self, body):  
+        
+        # dist = np.sqrt(((self.position[0]-body.position[0])**2)+((self.position[1]-body.position[1])**2)+((self.position[2]-body.position[2])**2))
+        # if(dist != 0):
+        #     self.acceleration = (-self.G*body.mass*(self.position-body.position))/(dist**3)
         dist = np.sqrt(((self.position[0]-body.position[0])**2)+((self.position[1]-body.position[1])**2)+((self.position[2]-body.position[2])**2))
-        if(dist != 0):
-            self.acceleration = (-self.G*body.mass*(self.position-body.position))/(dist**3)
+        self.acceleration += (-self.G*body.mass*(self.position-body.position))/(dist**3)
+        
 
     def kineticEnergy(self):
         K_Energy = 0.5*self.mass*(np.linalg.norm(self.velocity))**2
--- Simulation.py
+++ Simulation.py
@@ -2,24 +2,32 @@ import numpy as np
 from Particle import Particle
 import copy
 
+body_counter = 0
+bodies = []
+
 earthMass = 5.97237e24     # https://en.wikipedia.org/wiki/Earth
 earthRadius = 63710 * 1e3  # https://en.wikipedia.org/wiki/Earth
 Earth = Particle(
-    position=np.array([0, 0, 0]),
-    velocity=np.array([0, 0, 0]),
-    acceleration=np.array([0, 0, 0]),
-    name="Earth",
-    mass=earthMass
+    np.array([0, 0, 0]),
+    np.array([0, 0, 0]),
+    np.array([0, 0, 0]),
+    "Earth",
+    earthMass,
+    body_counter
 )
+bodies.append(Earth)
+
 satPosition = earthRadius + (35786 * 1e3)
 satVelocity = np.sqrt(Earth.G * Earth.mass / satPosition)  # from centrifugal force = gravitational force
 Satellite = Particle(
-    position=[satPosition, 0, 0],
-    velocity=[0, satVelocity, 0],
-    acceleration=np.array([0, 0, 0]),
-    name="Satellite",
-    mass=100.
+    [satPosition, 0, 0],
+    [0, satVelocity, 0],
+    np.array([0, 0, 0]),
+    "Satellite",
+    100.0,
+    body_counter
 )
+bodies.append(Satellite)
 
 time=0
 n=0
@@ -27,13 +35,47 @@ Data = []
 
 for a in range(200000):
     time=time+6
-    for b in [Earth, Satellite]:
-        for c in [Earth, Satellite]:
-            b.updateGravitationalAcceleration(c)
+    for b in bodies:
+        b.acceleration = []
+        for c in bodies:
+            if b.index != c.index:
+                b.updateGravitationalAcceleration(c)
         b.update(6)
-    #print(n)
-    #print(time)
+    print(n)
+    print(time)
     if (n%100 == 0) or (n == 0):
         Data.append([time, copy.deepcopy(Earth), copy.deepcopy(Satellite)])
     n=n+1
+
 np.save("TwoBodyTest", Data, allow_pickle=True)
+
+def print_particle(particle):
+    print("Particle: {}".format(particle.name))
+    print("  Mass: {0:.3e}, ".format(particle.mass))
+    for attribute in ["position", "velocity", "acceleration"]:
+        print("  {}: {}".format(attribute, particle.__getattribute__(attribute) + 0.0))  # add 0.0 to avoid negative zeros!
+
+
+print("The Earth and Satellites Location after {0} seconds is:".format((2000*6)))
+print_particle(Earth)
+print_particle(Satellite)
+if path.exists("TwoBodyTest.npy"):
+    print("The file TwoBodyTest.npy has been created.")
+
+print("testing reading it back in")
+DataIn = np.load("TwoBodyTest.npy", allow_pickle=True)
+
+print("Printing First Entry")
+print("{}".format(int(DataIn[0][0])))  #time
+print_particle(DataIn[0][1])  # Earth
+print_particle(DataIn[0][2])  # Satellite
+
+print("Printing Fifth Entry")
+print("{}".format(int(DataIn[4][0])))  #time
+print_particle(DataIn[4][1])  # Earth
+print_particle(DataIn[4][2])  # Satellite
+
+print("Printing Last Entry")
+print("{}".format(int(DataIn[-1][0])))  #time
+print_particle(DataIn[-1][1])  # Earth
+print_particle(DataIn[-1][2])  # Satellite
